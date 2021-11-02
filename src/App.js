1dd99e80ad0d3da9ee1e3ced482e41a58bf842e2
Caner Uçar <caner.ucar@akinon.com>
2021-11-03 00:27:19 +0300

    Add mobile burger menu

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.scss';
+import './Menu.scss';
 import CourseCard from './componets/course-card/CourseCard';
 import Home from './componets/home/Home';
 import Footer from 'componets/footer-ui/Footer';
+import { elastic as Menu } from 'react-burger-menu';
 
 export default function App() {
   return (
     <>
       <Router>
-        <div className="fd-wrapper">
-          <nav className="sidebar">
-            <ul className="sidebar__content">
-              <li className="sidebar__content-item">
-                <Link to="/" className="link">
-                  Home
-                </Link>
-              </li>
-              <li className="sidebar__content-item">
-                <Link to="/cours" className="link">
-                  Course Card
-                </Link>
-              </li>
-              <li className="sidebar__content-item">
-                <Link to="/footer" className="link">
-                  Footer UI
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <div className="content">
+        <div className="fd-wrapper" id="outer-container">
+          <Menu outerContainerId={ "outer-container" }>
+            <Link to="/" className="menu-item">Home</Link>
+            <Link to="/cours" className="menu-item">Course Card</Link>
+            <Link to="/footer" className="menu-item">Footer</Link>
+          </Menu>
+          <main className="content">
             <Switch>
               <Route path="/cours">
                 <CourseCard />
@@ -41,8 +29,8 @@ export default function App() {
                 <Home />
               </Route>
             </Switch>
+          </main>
           </div>
-        </div>
       </Router>
     </>
   );
